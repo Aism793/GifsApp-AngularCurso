cea7b3b4d92622128a2f92b813ea29830d1ab7b7
Aism793 <ismacenteno123@gmail.com>
2021-08-17 23:38:52 -0500

    Terminada

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,5 +1,6 @@
 import { ElementRef } from '@angular/core';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -11,10 +12,16 @@ export class BusquedaComponent{
 
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; //El ! me permite indicarle a typescript que el objeto nunca va a ser nulo
 
+  constructor(private gifsService: GifsService){}
+
   buscar(){
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    if(valor.trim().length === 0){
+      alert("Asegúrese de escribir algo para buscar");
+      return;
+    }
 
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = '';
   }
 
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent{
 
-  constructor() { }
+  get historiales(){
+    return this.gifsService.historial;
+  }
 
-  ngOnInit(): void {
+  constructor(private gifsService: GifsService) { }
+
+  buscar(termino: string){
+    console.log(termino);
+    this.gifsService.buscarGifs(termino);
   }
 
 }
